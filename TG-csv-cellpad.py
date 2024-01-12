6e9bdeefd13a941878312236c007a4d28b4f95bc
EdFife <103080265+EdFife@users.noreply.github.com>
2024-01-12 18:06:34 -0500

    Update TG-csv-cellpad.py
    Adjusted for variations in header rows and tightened up the code.

--- TG-csv-cellpad.py
+++ TG-csv-cellpad.py
@@ -13,50 +13,23 @@ from tkinter import filedialog
 
 root = tk.Tk()
 root.withdraw()
+starting_number = input("What number do you need to lead with for your configuration? TGIF usually is 3 for example: ") # Which DMR network are you configuring? 
+# BM does not need changing, TGIF usually is 3 for example. 
 filename = filedialog.askopenfilename() # Opens file dialog to select your file
 filepath = os.path.dirname(filename) # grab path for the original file so output files can be saved to same directory
-starting_number = input("What number do you need to lead with for your configuration? TGIF usually is 5 for example: ") # Which DMR network are you configuring? 
-# BM does not need changing, TGIF usually is 5 for example. 
 savefilename = (filepath + "/TG_" + starting_number + "_list.csv") # saving to same directory as opened file with filename of TG_x_list.csv.
 Errorfile = (filepath + "/TG_" + starting_number + "_Errorlog.csv") # Error log file saving to same directory with filename of TG_x_Errorlog.csv.
 
-with open(filename, 'r') as input_file, open("temp.csv", 'w+', newline='') as output_file2, open(savefilename, 'w+', newline='') as output_file: #opening  files to pad TG column, creates a temp.csv file to place bad TG numbers in
-    reader = csv.reader(input_file)
-    writer = csv.writer(output_file)
-    writer = csv.writer(output_file2)
+#inputfile = csv.reader((open(filename, 'r')), delimiter=',')
 
-    for row in reader:
-        if len(row[0]) < 7:
-            row[0] = starting_number + row[0].rjust(6, '0') # if cell is less than 7 add your prefix digit and pad cell to 7
-            writer = csv.writer(output_file)
-            writer.writerow(row) # write to TG_x_List.csv in same folder as original file
-          
-        elif len(row[0]) == 7:
-            writer = csv.writer(output_file2) # save "bad" talkgroups to temp file
-            writer.writerow(row) 
-        
-        else:
-            continue 
-            
-    input_file.close()
-    output_file.close()
-    output_file2.close()
-    
-with open("temp.csv", 'r+') as input_file, open(Errorfile, 'w', newline='') as output_file2, open(savefilename, 'a+', newline='') as output_file: # opening temp file to loop through bad files and find ones that are 7 long and start with your first character
-    reader = csv.reader(input_file) # reading the temp file
-    writer = csv.writer(output_file)
-    writer = csv.writer(output_file2)
-    
-    for row in reader:
-        if row[0].startswith(starting_number):
-            writer = csv.writer(output_file)
-            writer.writerow(row) # append to TG_x_List.csv in same folder as original file
-            
-        else:
-            writer = csv.writer(output_file2)
-            writer.writerow(row) # write to TG_x_Errorlog.csv in same folder as original file
-            
-            continue
-        
-os.remove("temp.csv")
-    
+for row in csv.reader((open(filename, 'r')), delimiter=','):
+    if len(row[0]) < 7:
+        row[0] = starting_number + row[0].rjust(6, '0') # if cell is less than 7 add your prefix digit and pad cell to 7
+        csv.writer(open(savefilename, 'a', newline='')).writerow(row) # write to TG_x_List.csv in same folder as original file
+    elif len(row[0]) == 7 and row[0].startswith(starting_number):
+        csv.writer(open(savefilename, 'a', newline='')).writerow(row) # append to TG_x_List.csv in same folder as original file
+    elif row[0].startswith("T"):
+        continue
+    else:
+        writer = csv.writer(open(Errorfile, 'a', newline='')) # outputs to TG_x_Errorlog.csv file
+        writer.writerow(row)
